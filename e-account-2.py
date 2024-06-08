4f023807a66bcbe8bb829e9b6cecb4e5237e338c
HPE División Empresarial de Ing. Blockchain <leonardodazablanco@gmail.com>
2024-06-08 16:15:34 -0400

    e-account-2.py

---
+++ e-account-2.py
@@ -0,0 +1,23 @@
+#elif (Validar_Tarjeta=="501878200078043777" and Nick=="7202"): #SEGUNDA CUENTA
+ # imprimir("*******************************")
+  # print(" BIENVENIDO ")
+  # imprimir("*******************************")
+opcion = int ( input ( 'Selecciona la Opcion que deseas:' ))
+
+    si ( opción == 1 ):
+        Validar_Usuario (501878200078043777) 
+
+    elif ( opción == 2 ):
+        Retirar_Monto () : print ( "saldo" , deposito,cuenta Mercantilbanco  )
+    elif ( opción == 3 ):
+        Consultar_saldo () 
+    elif ( opción == 4 ):
+    transferir_monto(), 
+    elif ( opción == 5 ):send =('enter') 
+        enviar_saldo_cuenta ():,cuenta Mercantilbanco:  (01050011740011554878) 
+    elif ( opción == 6 ):
+        print ( "Finalizando...Cerrando Aplicación" )
+        romper
+    demás :
+        print ( "Ese numero de Opcion no existe.. Intente de nuevo" )
+Menú () 
