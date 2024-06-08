ff5b13103f02cd6cf9d13919d8ab374ce0ceccee
HPE División Empresarial de Ing. Blockchain <leonardodazablanco@gmail.com>
2024-06-08 16:08:23 -0400

    comandos ATM.py
    python -m pip install keyboard. Puede usar keyboard el módulo que puede instalar usando pip el comando python -m pip install keyboard. from keyboard import press press('enter')

---
+++ comandos ATM.py
@@ -0,0 +1,17 @@
+( opción == 1 ):
+        Validar_Usuario (501878200078043777) 
+
+    elif ( opción == 2 ):
+        Retirar_Monto ()cuenta Mercantilbanco () 
+    elif ( opción == 3 ):
+        Consultar_saldo () 
+    elif ( opción == 4 ):
+    transferir_monto(), 
+    elif ( opción == 5 ):send =('enter') 
+        enviar_saldo_cuenta () ,cuenta Mercantilbanco: cta (01050011740011554878) 
+    elif ( opción == 6 ):
+        print ( "Finalizando...Cerrando Aplicación" )
+        romper
+    demás :
+        print ( "Ese numero de Opcion no existe.. Intente de nuevo" )
+Menú ()
