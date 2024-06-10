fb11a80a44b256a10ff77a72a986441e8c21dea0
HPE División Empresarial de Ing. Blockchain <leonardodazablanco@gmail.com>
2024-06-10 10:52:04 -0400

    e account.py

--- e account.py
+++ e account.py
@@ -5,7 +5,7 @@ def main():
         opcion = int(input("Seleccione una opción: "))
         if opcion == 7:
 {
-"owner_type":"USER"
+"owner_type":"bitpay_account_plus"
 "user_id":"usr-20I2tIqG3buTsvHKKORrtY2MkFH"
 "company_id":"cmp-bitpayMdUfg48A2H2rvoX7CV3toE0Z72fX"
 "country":"VE"
