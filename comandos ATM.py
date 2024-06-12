caeb66dff3003930cd41daa8e9eeff1931c01902
HPE División Empresarial de Ing. Blockchain <leonardodazablanco@gmail.com>
2024-06-12 11:29:10 -0400

    comandos ATM.py

--- comandos ATM.py
+++ comandos ATM.py
@@ -1,17 +1,18 @@
-( opción == 1 ):
+    opcion = int ( input ( 'Selecciona la Opcion que deseas:' ))
+
+    si ( opción == 1 ):
         Validar_Usuario (501878200078043777) 
 
     elif ( opción == 2 ):
-        Retirar_Monto ()cuenta Mercantilbanco () 
+        Retirar_Monto () : print ( "saldo" , "deposito","cuenta Mercantilbanco"  )
     elif ( opción == 3 ):
         Consultar_saldo () 
     elif ( opción == 4 ):
     transferir_monto(), 
     elif ( opción == 5 ):send =('enter') 
-        enviar_saldo_cuenta () ,cuenta Mercantilbanco: cta (01050011740011554878) 
+        enviar_saldo_cuenta ():,"cuenta Mercantilbanco":,(01050011740011554878) 
     elif ( opción == 6 ):
-        print ( "Finalizando...Cerrando Aplicación" )
+        print ( "Finalizando...Cerrando Aplicación" ) 
         romper
     demás :
         print ( "Ese numero de Opcion no existe.. Intente de nuevo" )
-Menú ()
